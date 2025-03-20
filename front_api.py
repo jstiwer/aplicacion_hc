b57efc2ee70d5d650ae0e33d5b3211066493e54f
Johhan Ramirez <jsra65@hotmail.com>
2025-03-20 18:47:36 -0500

    Update front_api.py

--- front_api.py
+++ front_api.py
@@ -4,13 +4,15 @@ import requests
 API_URL = "https://api1-zv36.onrender.com/chat"  # Replace with your API URL
 
 st.set_page_config(page_title="Chatbot HabiCredit", layout="wide")
-st.title("🏡 Habicredit Chatbot 1")
+st.title("🏡 Habicredit Chatbot 2")
 
-query = st.text_input("Realiza las preguntas que hacen tus brokers:")
+# Usa st.form para agrupar el input y botón
+with st.form(key='chat_form'):
+    query = st.text_input("Realiza las preguntas que hacen tus brokers:")
+    submit_button = st.form_submit_button(label='Enviar')
 
-if st.button(label='Enviar') and query:
+if submit_button and query:
     response = requests.get(API_URL, params={"query": query})
-    
 
     if response.status_code == 200:
         st.write("🔍 Respuesta:", response.json().get("response", "⚠️ No 'response' key found in JSON."))
