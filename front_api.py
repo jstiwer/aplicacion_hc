4bad4a491f1551c4e4924d37d7cedefeb3c46d5a
Johhan Ramirez <jsra65@hotmail.com>
2025-03-21 16:19:07 -0500

    Update front_api.py

--- front_api.py
+++ front_api.py
@@ -3,9 +3,6 @@ import requests
 
 API_URL = "https://api1-zv36.onrender.com/chat"  # Replace with your API URL
 
-# Ajusta el diseño con columnas
-col1 = st.columns([1, 1])
-
 with col1:
     st.image("logo_hc.png", width=200)  # Reemplaza con la URL de tu imagen
     st.markdown("<h1 style='text-align: center;'>Agente Habicredit</h1>", unsafe_allow_html=True)
