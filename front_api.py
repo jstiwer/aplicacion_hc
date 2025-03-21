b0e777bdcfa9f50ac5f446c377f7bc2c1412a4b1
Johhan Ramirez <jsra65@hotmail.com>
2025-03-20 19:17:30 -0500

    Update front_api.py

--- front_api.py
+++ front_api.py
@@ -11,7 +11,7 @@ with col1:
     st.image("image__6_-removebg-preview.png", width=300)  # Reemplaza con la URL de tu imagen
 
 with col2:
-    st.title("Agente")
+    st.title("Agente Directores y brokers")
     
 ## CSS
 st.markdown("""
