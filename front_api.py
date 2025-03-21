44d0c3a4f6f4c9e56b943cb0c7b6f925e79ffb92
Johhan Ramirez <jsra65@hotmail.com>
2025-03-20 19:12:12 -0500

    Update front_api.py

--- front_api.py
+++ front_api.py
@@ -8,7 +8,7 @@ col1, col2 = st.columns([1, 5])
 
 # Inserta la imagen en la primera columna y el título en la segunda
 with col1:
-    st.image("https://drive.google.com/drive/folders/1FaqOTvDRT_CiRke1E0X42ctQQxudk2IO", width=50)  # Reemplaza con la URL de tu imagen
+    st.image("image__6_-removebg-preview.png", width=50)  # Reemplaza con la URL de tu imagen
 
 with col2:
     st.title("🏡 Habicredit Chatbot 2")
